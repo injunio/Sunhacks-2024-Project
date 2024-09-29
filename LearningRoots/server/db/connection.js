500f07fbf41a9f988462a9a04e355c1ce6b176bd
albp67 <tonynguyenttd6775@gmail.com>
2024-09-29 01:14:06 -0700

    Initial commit

---
+++ LearningRoots/server/db/connection.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict:true,
+        deprecationErrors: true,
+    },
+
+});
+
+try {
+    await client.connect();
+
+    await client.db("admin").command({ping: 1});
+    console.log(
+        "Ping your development. You successfuly connected!"
+    );
+} catch(err) {
+    console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
